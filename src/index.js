a98a31be3fe943d9fd24e99061d02746da70c48e
ErickTrejoRobles <62369090+ErickTrejoRobles@users.noreply.github.com>
2020-06-01 17:23:51 -0500

    version 1.1.0

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 
+
+import './css/normalize.css'
 import './styles.css';
 import './css/mobile.css';
-import './css/normalize.css'
+
 import './js/componentes'
+import emailjs from 'emailjs-com';
+
 
 
 
@@ -12,8 +16,10 @@ import './js/componentes'
     // const rowGrap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-gap'));
     // const item = document.querySelectorAll('.item');
     // const rowSpan = Math.ceil((item[3].querySelector('.servicio').getBoundingClientRect().height + rowGrap) / (rowHeight+rowGrap));
+    const buttonEnviar = document.querySelector(".enviarCorreo"); 
 
-
+    emailjs.init("user_lMbk8zVmjWeQUnRI5se5k");
+    
     function resizeGridItem(item){
        
         const grid = document.getElementsByClassName("grid")[0];
@@ -47,3 +53,39 @@ import './js/componentes'
         imagesLoaded( allItems[x], resizeInstance);
       }
 
+
+
+      buttonEnviar.addEventListener('click',()=>{
+
+
+        const inputs = document.querySelectorAll(".input");
+        const txtarea = document.querySelector('.txtarea')
+
+
+        const tamplate = {
+          name : inputs[0].value,
+          empresa : inputs[1].value,
+          email_contacto : inputs[2].value,
+          message_html : txtarea.value
+
+
+
+          
+        }
+
+        console.log(tamplate);
+        emailjs.send("xiaomi","contacto_montajesc",tamplate).then(function(response) {
+
+          for(let i=0;i<inputs.length;i++){
+            inputs[i].value=""
+
+          }
+          txtarea.value="";
+          
+          console.log('SUCCESS!', response.status, response.text);
+       }, function(error) {
+         alert("No se ha enviado el mensaje, intentelo de nuevo")
+          console.log('FAILED...', error);});
+
+
+      })
